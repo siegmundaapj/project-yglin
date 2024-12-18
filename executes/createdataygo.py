422d0004ce0936ea78f38ae63aa6aa06b5520794
siegmundaapj <patrick.siegmund1@gmail.com>
2024-12-18 17:36:23 +0100

    add txt dump

--- executes/createdataygo.py
+++ executes/createdataygo.py
@@ -5,9 +5,11 @@ import csv
  #API call from ygoprodeck.com gathers card metadata
 
 base_url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
-params = {}
+params = {
+    "misc": "yes"} #for release date info
 csv_file = "ygocardlist.csv"
 excel_file = "ygocardlist.xlsx"
+txt_file = "ygotextdump.txt"
 
 def get_base_info():
     try:
@@ -27,17 +29,17 @@ def create_csv(card_data, filename=csv_file):
 
     with open(filename, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(["Name", "Description", "Race", "Type", "Attribute"])
-        
+        writer.writerow(["Name", "Description", "Race", "Type", "Attribute", "Release"]) 
         for card in card_data["data"]:
             name = card.get("name", "N/A")
             desc = card.get("desc", "N/A")
             race = card.get("race", "N/A")
-            type_ = card.get("type", "N/A")
-            attribute = card.get("attribute", "N/A")          
-            writer.writerow([name, desc, race, type_, attribute])
+            type = card.get("type", "N/A")
+            attribute = card.get("attribute", "N/A"),
+            release = card.get("misc_info", [{}])[0].get("tcg_date", "N/A")          
+            writer.writerow([name, desc, race, type, attribute, release])
 
-    print(f"CSV-file created.")       
+    print("csv file created.")       
     
 def create_excel(card_data, filename=excel_file):
     if "data" not in card_data:
@@ -48,26 +50,40 @@ def create_excel(card_data, filename=excel_file):
         name = card.get("name", "N/A")
         desc = card.get("desc", "N/A")
         race = card.get("race", "N/A")
-        type_ = card.get("type", "N/A")
-        attribute = card.get("attribute", "N/A")
-        
+        type = card.get("type", "N/A")
+        attribute = card.get("attribute", "N/A"),
+        release = card.get("misc_info", [{}])[0].get("tcg_date", "N/A")
         card_list.append({
             "Name": name,
             "Description": desc,
             "Race": race,
-            "Type": type_,
-            "Attribute": attribute
+            "Type": type,
+            "Attribute": attribute,
+            "Release": release
         })
-    
     base_dataframe = pd.DataFrame(card_list)
-    with pd.ExcelWriter(filename, engine="openpyxl") as writer:
-        base_dataframe.to_excel(writer, index=False)
+    with pd.ExcelWriter(filename, engine="openpyxl") as excel:
+        base_dataframe.to_excel(excel, index=False)
 
-    print(f"Excel-file created.")
+    print("xlsx file created.")
+
+def create_txt(card_data, filename=txt_file):
+    if "data" not in card_data:
+        return
+    with open(filename, "w", encoding="utf-8") as file:
+            cards = card_data.get("data", [])
+            for card in cards:
+                desc = card.get("desc", "")  
+                file.write(desc + "\n")  
+            print("txt file created.")
 
 
 if __name__ == "__main__":
+    # params = {"name": "Spright Blue",
+    #           "misc": "yes"}
     card_data = get_base_info()
+    #print(card_data)
     if card_data:
         create_csv(card_data)
         create_excel(card_data)
+        create_txt(card_data)
